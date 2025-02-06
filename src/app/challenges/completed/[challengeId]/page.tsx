ef344ac2a0818a26ab9c2aea437aff13f10c9b63
Philip Huynh <wuflerz@outlook.com>
2025-02-06 13:29:46 +0200

    Optimise params

--- src/app/challenges/completed/[challengeId]/page.tsx
+++ src/app/challenges/completed/[challengeId]/page.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
 import { getChallengeById } from '@/data/challenges/challenge'
 import {
   getSubmissions,
   getUserSubmission,
 } from '@/data/challenges/get-submissions'
 
-export type Props = {
-  params: {
-    challengeId: string
-  }
-}
-
-export default async function ChallengePage({ params }: Props) {
-  const { challengeId } = await params
-  const challenge = await getChallengeById(challengeId)
+export default async function ChallengePage({
+  params,
+}: {
+  params: Promise<{ challengeId: string }>
+}) {
+  const id = (await params).challengeId
+  const challenge = await getChallengeById(id)
 
   const submissions = await getSubmissions(challenge.id)
   const userSubmission = await getUserSubmission(challenge.id)
