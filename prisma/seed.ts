316a6f58e2c908bdab06caffbb7e4d3d34bb5805
Alex <115784039+z3kkeen@users.noreply.github.com>
2025-02-14 14:03:55 +0200

    challenges 9, 10 (#98)
    * 9, 10
    
    * missing )
    
    ---------
    
    Co-authored-by: Philip Huynh <wuflerz@outlook.com>

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,7 +10,7 @@ async function main() {
       email: 'email@gmail.com',
       name: 'Admin',
       emailVerified: true,
-      role: 'admin'
+      role: 'admin',
     },
   })
 
@@ -28,7 +28,7 @@ async function main() {
     expect(sayHello()).toBe('Hello World')
   })
 ])`,
-        userId: admin.id
+        userId: admin.id,
       },
       {
         title: 'Reverse String',
@@ -49,7 +49,7 @@ async function main() {
     expect(reverseString('a')).toBe('a')
   })
 ])`,
-        userId: admin.id
+        userId: admin.id,
       },
       {
         title: 'Case Insensitive String Comparison',
@@ -154,7 +154,8 @@ async function main() {
       },
       {
         title: 'Second Largest Number',
-        description: 'Write a function that returns the second largest number in an array of numbers.',
+        description:
+          'Write a function that returns the second largest number in an array of numbers.',
         level: 1,
         boilerplate: `function secondLargest(numbers) {\n  // Your code here\n}`,
         tips: `1. Consider sorting the array first
@@ -170,11 +171,12 @@ async function main() {
     expect(secondLargest([-5, -2, -10, -1])).toBe(-2)
   }),
 ])`,
-        userId: admin.id
+        userId: admin.id,
       },
       {
         title: 'Remove Duplicates',
-        description: 'Write a function that takes an array as an argument and returns a new array where all duplicates have been removed, so that each element only appears once. For example, if the input is [1,2,2,3,4,4,5], the function should return [1,2,3,4,5].',
+        description:
+          'Write a function that takes an array as an argument and returns a new array where all duplicates have been removed, so that each element only appears once. For example, if the input is [1,2,2,3,4,4,5], the function should return [1,2,3,4,5].',
         level: 0,
         boilerplate: `function removeDuplicates(arr) {\n  // Your code here\n}`,
         tips: `1. Consider using Set to remove duplicates
@@ -193,11 +195,12 @@ async function main() {
     expect(removeDuplicates([1, 1, 1, 1])).toEqual([1])
   })
 ])`,
-        userId: admin.id
+        userId: admin.id,
       },
       {
         title: 'Find Missing Number',
-        description: 'Write a function that takes an array of integers in ascending order and finds the missing number in the sequence. For example, if the array is [1,2,3,5], the function should return 4.',
+        description:
+          'Write a function that takes an array of integers in ascending order and finds the missing number in the sequence. For example, if the array is [1,2,3,5], the function should return 4.',
         level: 1,
         boilerplate: `function findMissingNumber(numbers) {\n  // Your code here\n}`,
         tips: `1. Consider that the numbers are in ascending order
@@ -216,11 +219,12 @@ async function main() {
     expect(findMissingNumber([1, 3])).toBe(2)
   })
 ])`,
-        userId: admin.id
+        userId: admin.id,
       },
       {
         title: 'Anagram Checker',
-        description: 'Write a function that takes two strings as arguments and determines if they are anagrams. Two strings are anagrams if they contain the same letters in the same quantity but in a different order. For example, "listen" and "silent" are anagrams. But "Hello" and "World" is not.',
+        description:
+          'Write a function that takes two strings as arguments and determines if they are anagrams. Two strings are anagrams if they contain the same letters in the same quantity but in a different order. For example, "listen" and "silent" are anagrams. But "Hello" and "World" is not.',
         level: 1,
         boilerplate: `function areAnagrams(str1, str2) {\n  // Your code here\n}`,
         tips: `1. Consider converting strings to lowercase to make comparison case-insensitive
@@ -240,9 +244,56 @@ async function main() {
     expect(areAnagrams('Debit', 'Bited')).toBe(true)
   })
 ])`,
-        userId: admin.id
-      }
-    ]
+        userId: admin.id,
+      },
+      {
+        title: 'Return Biggest Number',
+        description:
+          'Write a function that has an array with numbers as argument, and returns the largest number in the array. If the array, for example, is [3, 5, 8, 2, 7], the function should return 8.',
+        level: 1,
+        boilerplate: `function findLargestNumber(numbers) {\n // Your code here \n}`,
+        tips: `1.	Try using the javascript Math function to get the max number of the array.
+               2. You could also try using pre-existing functions like .reduce to get the same result.`,
+        tests: `describe('findLargestNumber', [
+                test('returns the largest number for a randomly generated array', () => {
+                  const numbers = Array.from({ length: 10 }, () => Math.floor(Math.random() * 101))
+                  const expected = Math.max(...numbers);
+                  expect(findLargestNumber(numbers)).toBe(expected)
+                }),
+                test('returns the largest number for a fixed array with negative and positive values', () => {
+                  const numbers = [-10, -20, -3, -50, 0];
+                  expect(findLargestNumber(numbers)).toBe(0)
+                })
+              ])`,
+        userId: admin.id,
+      },
+      {
+        title: 'Sum Of Array Elements',
+        description:
+          'Write a function that has an array of numbers as argument and returns the sum of all elements in the array. For example, if the array is [2, 4, 10, 1], the function should return 17.',
+        level: 1,
+        boilerplate: `function sumOfElements(arr) {\n // Your code here \n}`,
+        tips: `1. Consider using JavaScript's reduce() method to iterate over the array and calculate the sum.
+               2. Write or imagine a few test cases (e.g., a fixed array with known values, a random array, and even negative numbers) before you code. This helps you confirm that your solution works for a variety of scenarios.`,
+        tests: `describe('sumOfElements', [
+                test('return the sum for a randomly generated array', () => {
+                  const amount = Math.floor(Math.random() * (10 - 5 + 1)) + 5
+                  const arr = []
+                  for (let i = 0; i < amount; i++) {
+                    arr.push(Math.floor(Math.random() * (100 - 0 + 1)) + 0)
+                  }
+                  const expected = arr.reduce((acc, curr) => acc + curr, 0)
+                  expect(sumOfElements(arr)).toBe(expected)
+                }),
+                test('return the correct sum for a fixed array [2, 4, -10, 0]', () => {
+                  const arr = [2, 4, -10, 0];
+                  const expected = -4;
+                  expect(sumOfElements(arr)).toBe(expected);
+                }),
+              ])`,
+        userId: admin.id,
+      },
+    ],
   })
 }
 
